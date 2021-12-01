9c36aa1948abd9131a295537ec52270e0aab1075
jmccord9 <jmccord9@gatech.edu>
2021-11-30 22:18:27 -0500

    Updated DOS to plot correctly.

--- plotDOS.py
+++ plotDOS.py
@@ -1,15 +1,10 @@
-import re
 import numpy as np
 import os
 import math
 import matplotlib.pyplot as plt
-from scipy.integrate import simps
 import xml.etree.ElementTree as ET
 
-eV2Ha = 1 / 27.21138397
-n_electrons = 383
-HOMO_index = int(math.floor(n_electrons/2))
-LUMO_index = HOMO_index + 1
+eV2Ha = 1/27.21138397
 
 _VALS = {'max_values': [],
          'min_values': [],
@@ -18,66 +13,70 @@ _VALS = {'max_values': [],
          'weight_list': [],
          'eigenvalue_master': [],
          'E_min': None,
-         'E_max': None}
+         'E_max': None,
+         'E_fermi': None,
+         'HOMO_index': None,
+         'LUMO_index': None,
+         'n_electrons': None}
 
-def get_fermi_energy(filepath):
-    # write function that takes in pw.out file and returns Fermi energy
-    # of most converged slab
-    return 1.6017
+def parse_pw_out(filepath):
+    # Write code that parses pw.out file, extracts relevant values,
+    # and appends calculations to the _VALS dictionary.
+    _VALS['E_fermi'] = 1.6017
+    _VALS['n_electrons'] = 383
+    _VALS['HOMO_index'] = int(math.ceil(_VALS['n_electrons']/2)) -1
+    _VALS['LUMO_index'] = _VALS['HOMO_index'] + 1
+    return
 
 def read_surface_file_xml(filepath):
-
+    # Parse QuantumEspresso output xml file to get k-points information.
+    # Assumes standard QuantumEspresso xml tree structure and names.
     tree = ET.parse(filepath)
     root = tree.getroot()
-
+    # Search through all of child nodes to find k-point information.
     ks_energy_roots = []
     for descendant in root.iter():
         if descendant.tag == 'ks_energies':
             ks_energy_roots.append(descendant)
-
+    # Iterate through every k-point state to extract relevant information.
     for energy_root in ks_energy_roots:
         for child in energy_root:
-            # find k-point weight
+            # find k-point weights
             if child.tag == 'k_point':
                 _VALS['weight_list'].append(float(child.attrib['weight']))
             # extract all eigenvalues for each k-point
             if child.tag == 'eigenvalues':
-                eigen_values = child.text.split()
-                eigen_values = [float(i) for i in eigen_values]
-                _VALS['min_values'].append(min(eigen_values))
-                _VALS['max_values'].append(max(eigen_values))
-                _VALS['HOMO_values'].append(eigen_values[HOMO_index])
-                _VALS['LUMO_values'].append(eigen_values[LUMO_index])
+                eigen_values = np.array(child.text.split()).astype(float)
+                # eigen_values = [float(i) for i in child.text.split()]
+                _VALS['min_values'].append(np.amin(eigen_values,axis=0))
+                _VALS['max_values'].append(np.amax(eigen_values,axis=0))
+                _VALS['HOMO_values'].append(eigen_values[_VALS['HOMO_index']])
+                _VALS['LUMO_values'].append(eigen_values[_VALS['LUMO_index']])
                 _VALS['eigenvalue_master'].append(eigen_values)
 
     _VALS['E_min'] = min(_VALS['min_values'])/eV2Ha
     _VALS['E_max'] = max(_VALS['max_values'])/eV2Ha
-        
     return
 
 def calculate_DOS(eigenvalue_list, E_max, E_min):
-
-    qe_lambda_eV = [i*eV2Ha for i in eigenvalue_list]
-    # qe_lambda_eV = eigenvalue_list*eV2Ha
+    # Calculate the DOS using the list of eigenvalues (eigenvalue_master).
+    qe_lambda_eV = eigenvalue_list/eV2Ha # I don't think this needs to be multiplied...
+    # qe_lambda_eV = [i*eV2Ha for i in eigenvalue_list]
     mu = 0.05 #in eV
-    N = 20*len(qe_lambda_eV)
-
-    DOS_eV, E_eV = eig2DOS_bulk(qe_lambda_eV, E_min, E_max, N, mu) # swapped emin and emax
-
+    # N = 20*len(qe_lambda_eV)
+    N = 20*np.size(qe_lambda_eV,0)
+    DOS_eV, E_eV = eig2DOS_bulk(qe_lambda_eV, E_min, E_max, N, mu)
     return DOS_eV, E_eV
 
-def eig2DOS_bulk(lambda_val, E_min, E_max, num, sigma): #send the first eigen value 
-    
-    #we are sampling between minimum and maximum values of lambda
+def eig2DOS_bulk(lambda_val, E_min, E_max, num, sigma):
+    # Sample between minimum and maximum values of lambda.
     E = np.linspace(E_min,E_max,num)
     x = colminusrow(E,lambda_val)
-
     DOS = np.sum(gauss_distribution(x,sigma), axis=1)
-    
     return DOS, E
 
-#function to generate gaussian distribution using output from colminusrow and sigma value
 def gauss_distribution(x, s):
+    # Generate Gaussian distribution from variance and energy distribution values.
     p1 = -0.5*((x)/s)**2
     p2 = (s * np.sqrt(2*np.pi))
     f = np.exp(p1)/p2
@@ -97,12 +96,12 @@ def colminusrow(x, y):
 
 if __name__ == '__main__':
 
-    base = '/Users/jamesmccord/Dropbox (GaTech)/pBlock/fall2021/final_data/N/Base'
-    file = 'data-file-schema.xml'
-    file_path = os.path.join(base,file)
+    base = '/Users/jamesmccord/Dropbox (GaTech)/pBlock/fall2021/final_data/N/Base/'
+    xml_file, out_file = 'data-file-schema.xml', 'pw.out'
+    xml_path, out_path = os.path.join(base,xml_file), os.path.join(base,out_file)
 
-    E_fermi = get_fermi_energy(file_path)
-    read_surface_file_xml(file_path)
+    parse_pw_out(out_path)
+    read_surface_file_xml(xml_path)
 
     DOS_list = []
     for i in range(len(_VALS['eigenvalue_master'])):
@@ -111,25 +110,15 @@ if __name__ == '__main__':
         DOS_list.append(DOS_eV*weight)
 
     DOS_array = np.array(DOS_list)
-    DOS = np.sum(DOS_array, axis=0)
+    DOS = np.sum(DOS_array, axis=0)/_VALS['n_electrons']
 
     plt.plot(E_eV, DOS)
+    plt.axvline(x=_VALS['E_fermi'], color='cyan', label='Fermi level')
+    plt.axvline(x=np.max(_VALS['HOMO_values'])/eV2Ha, color='black', label='HOMO')
+    plt.axvline(x=np.min(_VALS['LUMO_values'])/eV2Ha, color='green', label='LUMO')
+    # plt.xlim([-10, 10])
+    plt.legend()
+    plt.xlabel('Energy (eV)')
+    plt.ylabel('Density of States')
     plt.show()
-
-#
-# DOS_array_100 = np.array(DOS_weighted_list_100)
-# DOS_100 = np.sum(DOS_array_100, axis = 0)
-# HOMO_value_100 = np.max(HOMO_values_100)*27.2114
-# LUMO_value_100 = np.min(LUMO_values_100)*27.2114
-#
-# plt.figure(figsize=(20,10))
-# #plt.xlim([0,20])
-# plt.ylim([0,150])
-# plt.plot(E_eV_100, DOS_100)
-# plt.axvline(x=E_fermi_100, color='cyan', label='Fermi level')
-# plt.axvline(x=HOMO_value_100, color='black', label='HOMO')
-# plt.axvline(x=LUMO_value_100, color='green', label='LUMO')
-# plt.xlabel('Energy (eV)')
-# plt.ylabel('Density of States')
-# plt.legend()
 
